abc3dd6e882331fd2f62e120dfe34985944344f0
namhla1221 <namhlamthi21@gmail.com>
2020-07-14 16:53:11 +0200

    new changes

--- dom/text-bill-dom.js
+++ dom/text-bill-dom.js
@@ -2,38 +2,17 @@ const billTypeTextElem = document.querySelector(".billTypeText");
 const addToBillBtnElem = document.querySelector(".addToBillBtn");
 const callTotalElem = document.querySelector(".callTotalOne");
 const smsTotalElem = document.querySelector(".smsTotalOne");
-const totalElem = documemt.querySelector(".totalOne");
-
-
-
+const totalElem = document.querySelector(".totalOne");
 const textBill = calculateTextBill();
-
-
-
 
 function buttonClicked() {
 
-
   var billTypeEntered = billTypeTextElem.value.trim();
-
-
   textBill.textBillTotal(billTypeEntered)
-
-
-  talElem.innerHTML = textBill.getCallTotal();
-
-
+  totalElem.innerHTML = textBill.getCallTotal();
   smsTotalElem.innerHTML = textBill.getSmsTotal();
-
-
-  
   totalElem.classList.add(textBill.styleColor());
-
-
-
-       totalElem.innerHTML = textBill.getTotal();
-
-
+  totalElem.innerHTML = textBill.getTotal();
 
 }
 
